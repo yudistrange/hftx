0543354c9bde4e300b289a3a1cfbd8060983f783
udit <yudistrange@gmail.com>
2023-01-05 23:22:29 +0530

    Update agent-worker docs

--- lib/hftx/agent/worker.ex
+++ lib/hftx/agent/worker.ex
@@ -1,4 +1,8 @@
 defmodule Hftx.Agent.Worker do
+  @moduledoc """
+  Worker process that emulates a trader. The worker is designed as an FSM using the gen_statem behaviour.
+  Each worker process will operate on one symbol with one strategy. These can be set at the time of the initialization of the process.
+  """
   @behaviour :gen_statem
 
   alias Hftx.Data.Agent.State, as: AgentState
@@ -14,26 +18,6 @@ defmodule Hftx.Agent.Worker do
   @impl true
   def init(data) do
     {:ok, :init, data}
-  end
-
-  def handle_event(_caller, event, _state, _data) do
-    IO.inspect(event, label: "Unhandled event")
-    {:keep_state_and_data, []}
-  end
-
-  def handle_event(_caller, event, _state, _data) do
-    IO.inspect(event, label: "Unhandled event")
-    {:keep_state_and_data, []}
-  end
-
-  def handle_event(_caller, event, _state, _data) do
-    IO.inspect(event, label: "Unhandled event")
-    {:keep_state_and_data, []}
-  end
-
-  def handle_event(_caller, event, _state, _data) do
-    IO.inspect(event, label: "Unhandled event")
-    {:keep_state_and_data, []}
   end
 
   def handle_event(_caller, event, _state, _data) do
