6747304a3cf935b741af15a6c50764c608b72e89
udit <yudistrange@gmail.com>
2023-01-05 23:35:56 +0530

    Add docs and update the transformer behaviour

--- lib/hftx/data/transformer/transformer.ex
+++ lib/hftx/data/transformer/transformer.ex
@@ -1,9 +1,11 @@
-defmodule Hftx.Data.Transformer do 
-@moduledoc """
-Describes the behaviour for various data compression strategies.
-Compression strategies are used to aggregate the Market Price data into compact form.
-"""
+defmodule Hftx.Data.Transformer do
+  @moduledoc """
+  Describes the behaviour for various data compression strategies.
+  Compression strategies are used to aggregate the Market Price data into compact form.
+  """
+  alias Hftx.Data.Market.Event, as: MarketEvent
+  alias Hftx.Data.Aggregate.CandleStick
 
-# TODO: Fix the update behaviour
-@callback update() : :ok
+  # TODO: Handle other aggregate structs when they are added
+  @callback update(market_event :: MarketEvent.t()) :: CandleStick.t()
 end
