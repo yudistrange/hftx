ccc2d18a4adbc6f2736995559c6e038bd46df18d
udit <yudistrange@gmail.com>
2023-01-12 12:58:22 +0530

    Fix: Use the correct env name for getting host and port for test db in test config

--- config/test.exs
+++ config/test.exs
@@ -8,9 +8,9 @@ import Config
 config :hftx, Hftx.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: System.get_env(POSTGRES_HOST) || "localhost",
+  hostname: System.get_env("POSTGRES_HOST") || "localhost",
   database: "hftx_test",
-  port: System.get_env(POSTGRES_PORT) || 25432,
+  port: System.get_env("POSTGRES_PORT") || 25432,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
