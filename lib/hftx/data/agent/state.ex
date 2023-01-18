1e3e3d913c619c204c5c2c48beb48824936d4b17
udit <yudistrange@gmail.com>
2023-01-18 16:56:27 +0530

    Add more logic to the Agent Workers module

--- lib/hftx/data/agent/state.ex
+++ lib/hftx/data/agent/state.ex
@@ -2,35 +2,16 @@ defmodule Hftx.Data.Agent.State do
   @moduledoc """
   Struct to represent the state of an agent worker
   """
+  alias Hftx.Data.Aggregate
+
   @enforce_keys [:strategy, :symbol]
-  defstruct [:name, :strategy, :symbol, events: [], trades: []]
+  defstruct [:name, :strategy, :symbol, :instrument_id, events: []]
 
   @type t :: %__MODULE__{
           name: String.t(),
           strategy: module,
           symbol: String.t(),
-          events: [__MODULE__.Events.t()],
-          trades: [__MODULE__.Trades.t()]
+          instrument_id: String.t(),
+          events: [Aggregate.t()]
         }
-
-  defmodule Events do
-    @enforce_keys [:timestamp, :price]
-    defstruct [:timestamp, :price]
-
-    @type t :: %__MODULE__{
-            timestamp: non_neg_integer(),
-            price: float()
-          }
-  end
-
-  defmodule Trades do
-    @enforce_keys [:count, :price, :type]
-    defstruct [:count, :price, :type]
-
-    @type t :: %__MODULE__{
-            count: non_neg_integer(),
-            price: float(),
-            type: Buy | Sell
-          }
-  end
 end
--- lib/hftx/workers/agent.ex
+++ lib/hftx/workers/agent.ex
@@ -1,4 +1,4 @@
-defmodule Hftx.Agent.Worker do
+defmodule Hftx.Workers.Agent do
   @moduledoc """
   Worker process that emulates a trader. The worker is designed as an FSM using the gen_statem behaviour.
   Each worker process will operate on one symbol with one strategy. These can be set at the time of the initialization of the process.
@@ -6,9 +6,32 @@ defmodule Hftx.Agent.Worker do
   @behaviour :gen_statem
 
   alias Hftx.Data.Agent.State, as: AgentState
+  alias Hftx.Data.Aggregate
 
-  def start_link({name, strategy, symbol}, opts) do
-    data = %AgentState{name: name, strategy: strategy, symbol: symbol}
+  @spec group_name(String.t()) :: String.t()
+  def group_name(symbol) do
+    "AgentGroup-" <> symbol
+  end
+
+  @spec name({module, String.t()}) :: String.t()
+  def name({strategy, symbol}) do
+    "Agent-" <> (strategy |> Atom.to_string()) <> "-" <> symbol
+  end
+
+  @spec observe({module, Aggregate.t()}) :: :ok
+  def observe({symbol, event}) do
+    # TODO: Get Pids from
+    :gen_statem.cast(symbol, {:observe, event})
+  end
+
+  def start_link({name, strategy, symbol, instrument_id}, opts) do
+    data = %AgentState{
+      name: name,
+      strategy: strategy,
+      symbol: symbol,
+      instrument_id: instrument_id
+    }
+
     :gen_statem.start_link(__MODULE__, data, opts)
   end
 
@@ -21,8 +44,7 @@ defmodule Hftx.Agent.Worker do
   end
 
   @impl true
-  def handle_event(_caller, event, _state, _data) do
-    IO.inspect(event, label: "Unhandled event")
+  def handle_event(_caller, _event, _state, _data) do
     {:keep_state_and_data, []}
   end
 end
