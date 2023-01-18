38b2a5e78dbaf534f2c5180573594b24bc686f27
udit <yudistrange@gmail.com>
2023-01-18 17:09:01 +0530

    Add Workers Module doc

---
+++ lib/hftx/workers/workers.ex
@@ -0,0 +1,9 @@
+defmodule Hftx.Workers.Workers do
+  @moduledoc """
+  Module containing the definitions for the various worker processes
+
+  - DataTransformer
+  - Agent
+  - DecisionMaker
+  """
+end
