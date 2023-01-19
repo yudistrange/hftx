902c39d9553bb057574b0d9d8f8a000bac5bea42
udit <yudistrange@gmail.com>
2023-01-19 23:53:43 +0530

    Update DecisionMaker strategy base behaviour to also account for AgentStrategy

--- test/hftx/strategies/decision_maker/naive_test.exs
+++ test/hftx/strategies/decision_maker/naive_test.exs
@@ -5,7 +5,10 @@ defmodule Hftx.Strategies.DecisionMaker.NaiveTest do
   alias Hftx.Data.Agent.Suggestion, as: AgentSuggestion
 
   test "Decide based on first agent's suggestion" do
-    assert Naive.decide([AgentSuggestion.long(), AgentSuggestion.hold()]) ===
+    assert Naive.decide([
+             {AgentStrat, AgentSuggestion.long()},
+             {AgentStrat, AgentSuggestion.hold()}
+           ]) ===
              AgentSuggestion.long()
   end
 end
