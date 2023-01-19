197de1751241d49c72120c0a0c07d5f9a7433966
udit <yudistrange@gmail.com>
2023-01-19 23:14:10 +0530

    Update the Agent Strategies behaviour to have a historical_event_window fn
    This fn will determine the maximum size of historical event data in the
    Agent's state. This ensures that the Agent don't consume infinite memory

--- test/hftx/strategies/agent/always_long_test.exs
+++ test/hftx/strategies/agent/always_long_test.exs
@@ -8,6 +8,24 @@ defmodule Hftx.Strategies.Agent.AlwaysLongTest do
   test "Evaluate the AlwaysLong agent strategy for CandleStick data" do
     market_events = [%CandleStick{open: 100, close: 50, high: 100, low: 50}]
 
-    assert AlwaysLong.evaluate(market_events) === AgentSuggestion.long()
+    assert AlwaysLong.evaluate(AgentSuggestion.long(), market_events) ===
+             {AgentSuggestion.long(), market_events}
+
+    assert AlwaysLong.evaluate(AgentSuggestion.short(), market_events) ===
+             {AgentSuggestion.long(), market_events}
+
+    assert AlwaysLong.evaluate(AgentSuggestion.hold(), market_events) ===
+             {AgentSuggestion.long(), market_events}
+  end
+
+  test "The event list never exceeds the historical_event_window" do
+    market_event = %CandleStick{open: 100, close: 50, high: 100, low: 50}
+    market_events = for _ <- 1..100, do: market_event
+
+    {suggestion, updated_market_events} =
+      AlwaysLong.evaluate(AgentSuggestion.long(), market_events)
+
+    assert AgentSuggestion.long() === suggestion
+    assert AlwaysLong.historical_event_window() === Enum.count(updated_market_events)
   end
 end
