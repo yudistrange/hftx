c7114ace5fe2a318203ecf4d8db76dbaabd1e9b0
udit <yudistrange@gmail.com>
2023-10-28 19:37:24 +0530

    Add support to parse full quote message with market depth information

--- lib/hftx/data/market_event/market_event.ex
+++ lib/hftx/data/market_event/market_event.ex
@@ -15,6 +15,11 @@ defmodule Hftx.Data.MarketEvent do
     :high_price,
     :low_price,
     :close_price,
+    :last_trade_timestamp,
+    :open_interest,
+    :open_interest_day_high,
+    :open_interest_day_low,
+    :exchange_timestamp,
     :order_book
   ]
 
@@ -32,12 +37,27 @@ defmodule Hftx.Data.MarketEvent do
           high_price: Float | nil,
           low_price: Float | nil,
           close_price: Float | nil,
+          last_trade_timestamp: non_neg_integer() | nil,
+          open_interest: non_neg_integer() | nil,
+          open_interest_day_high: non_neg_integer() | nil,
+          open_interest_day_low: non_neg_integer() | nil,
+          exchange_timestamp: non_neg_integer() | nil,
           order_book: __MODULE__.OrderBook.t() | nil
         }
 
   defmodule OrderBook do
+    @moduledoc false
+
     @enforce_keys [:bid, :offer]
     defstruct [:bid, :offer]
+
+    @typedoc """
+    OrderBook struct defines the structure of an order book
+
+    ## Fields
+      * `:bid` - List of current top bids 
+      * `:offer` - List of current top offers
+    """
 
     @type t :: %__MODULE__{
             bid: [__MODULE__.OrderTuple.t()],
@@ -45,12 +65,23 @@ defmodule Hftx.Data.MarketEvent do
           }
 
     defmodule OrderTuple do
-      @enforce_keys [:volume, :price]
-      defstruct [:volume, :price]
+      @moduledoc false
 
+      @enforce_keys [:quantity, :orders, :price]
+      defstruct [:quantity, :orders, :price]
+
+      @typedoc """
+      OrderTuple struct defines the structure of individual entries in an order book
+
+      ## Fields
+        * `:quantity` - The total quantity of stocks under bid / offer
+        * `:price` - The price for the bid / offer.
+        * `:orders` - Total number of orders that are operating at this price.
+      """
       @type t :: %__MODULE__{
-              volume: non_neg_integer(),
-              price: non_neg_integer()
+              quantity: non_neg_integer(),
+              price: non_neg_integer(),
+              orders: non_neg_integer()
             }
     end
   end
--- test/hftx/zerodha/web_socket/frame_test.exs
+++ test/hftx/zerodha/web_socket/frame_test.exs
@@ -2,6 +2,7 @@ defmodule Hftx.Zerodha.WebSocket.FrameTest do
   use ExUnit.Case
 
   alias Hftx.Zerodha.WebSocket.Frame
+  alias Hftx.Data.MarketEvent
 
   @quote_frame <<0, 1, 0, 44, 0, 6, 58, 1, 0, 2, 32, 141, 0, 0, 0, 97, 0, 2, 29, 80, 0, 56, 101,
                  31, 0, 3, 125, 117, 0, 4, 106, 217, 0, 2, 30, 38, 0, 2, 32, 236, 0, 2, 25, 228,
@@ -53,6 +54,49 @@ defmodule Hftx.Zerodha.WebSocket.FrameTest do
   end
 
   test "Parse binary full message as market_event" do
-    assert {:market_event, []} === Frame.parse(@full_frame)
+    frozen_ts = DateTime.utc_now()
+    {:market_event, [market_event]} = Frame.parse(@full_frame)
+
+    assert market_event |> Map.put(:timestamp, frozen_ts) === %MarketEvent{
+             average_price: 138_583,
+             close_price: 137_835,
+             high_price: 139_500,
+             instrument_token: 408_065,
+             last_trade_price: 139_300,
+             last_trade_volume: 5,
+             low_price: 137_700,
+             open_price: 138_790,
+             symbol: "",
+             total_buy_calls: 0,
+             total_sell_calls: 1240,
+             total_trade_calls: nil,
+             total_trade_volume: 3_729_093,
+             timestamp: frozen_ts,
+             last_trade_timestamp: 1_691_404_154,
+             open_interest: 0,
+             open_interest_day_high: 0,
+             open_interest_day_low: 0,
+             exchange_timestamp: 1_691_414_161,
+             order_book: %Hftx.Data.MarketEvent.OrderBook{
+               bid: [
+                 %Hftx.Data.MarketEvent.OrderBook.OrderTuple{quantity: 0, orders: 0, price: 0},
+                 %Hftx.Data.MarketEvent.OrderBook.OrderTuple{quantity: 0, orders: 0, price: 0},
+                 %Hftx.Data.MarketEvent.OrderBook.OrderTuple{quantity: 0, orders: 0, price: 0},
+                 %Hftx.Data.MarketEvent.OrderBook.OrderTuple{quantity: 0, orders: 0, price: 0},
+                 %Hftx.Data.MarketEvent.OrderBook.OrderTuple{quantity: 0, orders: 0, price: 0}
+               ],
+               offer: [
+                 %Hftx.Data.MarketEvent.OrderBook.OrderTuple{
+                   quantity: 1240,
+                   orders: 28,
+                   price: 139_300
+                 },
+                 %Hftx.Data.MarketEvent.OrderBook.OrderTuple{quantity: 0, orders: 0, price: 0},
+                 %Hftx.Data.MarketEvent.OrderBook.OrderTuple{quantity: 0, orders: 0, price: 0},
+                 %Hftx.Data.MarketEvent.OrderBook.OrderTuple{quantity: 0, orders: 0, price: 0},
+                 %Hftx.Data.MarketEvent.OrderBook.OrderTuple{quantity: 0, orders: 0, price: 0}
+               ]
+             }
+           }
   end
 end
