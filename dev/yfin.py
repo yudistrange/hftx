6e6d30678adc30b3fb48b58f838612c096fb88e7
udit <yudistrange@gmail.com>
2024-09-05 11:12:19 -0400

    Format and remove the unused deps from yahoo finance script

--- dev/yfin.py
+++ dev/yfin.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 import datetime
-import pandas as pd
-from dateutil.relativedelta import relativedelta
 import yfinance as yf
+
 
 def download(symbol="BAJFINANCE.NS", fname="download.csv"):
     sdate = datetime.date.today() - datetime.timedelta(days=30)
@@ -16,12 +15,13 @@ def download(symbol="BAJFINANCE.NS", fname="download.csv"):
     start = smonday
     end = sfriday
 
-    while(start < edate and end < edate):
+    while (start < edate and end < edate):
         bj_download = yf.download(symbol, start=start, end=end, interval="1m")
         print(bj_download)
         bj_download.to_csv(path_or_buf=fname, mode="a")
         start = start + datetime.timedelta(weeks=1)
         end = end + datetime.timedelta(weeks=1)
 
+
 if __name__ == "__main__":
-   download()
+    download()
